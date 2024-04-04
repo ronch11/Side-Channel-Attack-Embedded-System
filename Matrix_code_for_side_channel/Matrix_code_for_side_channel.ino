2c503873b6366ce1897116028915b6bdb696763d
ronch11 <71279601+ronch11@users.noreply.github.com>
2024-04-04 10:21:12 +0300

    Update Matrix_code_for_side_channel.ino

--- Matrix_code_for_side_channel/Matrix_code_for_side_channel.ino
+++ Matrix_code_for_side_channel/Matrix_code_for_side_channel.ino
@@ -1,13 +1,13 @@
 // Include the Keypad library
 #include <Keypad.h>
 
+// Declare the PWM pin for the buzzer
+const int buzzerPin = 12;
 
-const int buzzerPin = 12; // declaring the PWM pin</p><p>void setup() {
- 
 // Constants for row and column sizes
 const byte ROWS = 4;
 const byte COLS = 4;
- 
+
 // Array to represent keys on keypad
 char hexaKeys[ROWS][COLS] =
 {
@@ -17,38 +17,41 @@ char hexaKeys[ROWS][COLS] =
   {'*', '0', '#', 'D'}
 };
 
- 
 // Connections to Arduino
 byte rowPins[ROWS] = {9, 8, 7, 6};
 byte colPins[COLS] = {5, 4, 3, 2};
- 
+
 // Create keypad object
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
- 
+
+// Setup function for the Arduino
 void setup()
 {
   // Setup serial monitor
   Serial.begin(9600);  
 }
- 
+
+// Main loop function
 void loop()
 {
   // Get key value if pressed
   char customKey = customKeypad.getKey();
- 
+
+  // If a key is pressed
   if (customKey) {
     
     // Print key value to serial monitor
     Serial.println(customKey);
 
+    // Make a sound corresponding to the key
     makeButtonSound(customKey);
   }
 }
 
-// 400, 300, 2000, 
-
+// Function to make a sound corresponding to the button pressed
 void makeButtonSound(char button)
 {
+  // Each button corresponds to a different frequency
   if (button == '1') { return makeSound(1000); }
   if (button == '2') { return makeSound(500); }
   if (button == '3') { return makeSound(600); }
@@ -59,10 +62,9 @@ void makeButtonSound(char button)
   if (button == '8') { return makeSound(1200); }
   if (button == '9') { return makeSound(1400); }
   if (button == '0') { return makeSound(1300); }
-//  if (button == 'A') { return makeSound(); }
-//  if (button == 'B') { return makeSound(); }
   if (button == 'C') { return makeSound(29); }
   if (button == 'D') { 
+    // For button 'D', make a sequence of sounds
     for (int i=20; i<30; i++){
       makeSound(i);
       delay(80);
@@ -70,12 +72,15 @@ void makeButtonSound(char button)
   }
 }
 
-
-//
+// Function to make a sound at a given frequency
 void makeSound(int indexOfBeep)
 {
+  // Start the tone
   tone(buzzerPin, indexOfBeep);
+  // Wait for a while
   delay(750);
+  // Stop the tone
   noTone(buzzerPin);
+  // Wait for a while
   delay(100);
 }
