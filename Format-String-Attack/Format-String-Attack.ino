c6f5c34e2da9afa66cbd6df36d21d1277d68e10f
ronch11 <71279601+ronch11@users.noreply.github.com>
2024-04-04 10:18:15 +0300

    Update Format-String-Attack.ino

--- Format-String-Attack/Format-String-Attack.ino
+++ Format-String-Attack/Format-String-Attack.ino
@@ -1,40 +1,42 @@
+// Define the length of the buffer and format arrays
 #define LENGTH 100
 
-
+// Declare the buffer and format arrays
 char buff[LENGTH];
 char format[LENGTH];
 
-
+// Function prototypes
 void PrintWellcomeMessage();
 void inputFormat();
 void PrintResult(char* format);
 
-
+// The password to be checked
 char pass[] = "Fundmentals_Of_Software_Security";
 
-
-void setup()
-{
+// Setup function for the Arduino
+void setup() {
+    // Begin serial communication at 9600 baud
     Serial.begin(9600);
 }
 
-
-void loop()
-{
+// Main loop function
+void loop() {
+    // Print the welcome message
     PrintWellcomeMessage();
     delay(1000);
 
+    // Input the format string
     inputFormat();
     delay(1000);
     
+    // Print the result of the password check
     PrintResult();
 
     delay(1000);
 }
 
-
-void PrintWellcomeMessage()
-{
+// Function to print the welcome message
+void PrintWellcomeMessage() {
     String welcomeMessage = "Welcome to format string CTF. Try to read the password from the stack";
     Serial.println(welcomeMessage);
     Serial.println("Enter the length of format string you want write");
@@ -42,118 +44,38 @@ void PrintWellcomeMessage()
     delay(1000);
 }
 
-
-void inputFormat()
-{
+// Function to input the format string
+void inputFormat() {
+    // Wait until there is available data
     while (Serial.available() == 0) {}
 
+    // Parse the length of the format string
     int len = Serial.parseInt();
     
     delay(10000);
     Serial.println("Enter your format string:");
     delay(1000);
 
+    // Wait until there is available data
     while (Serial.available() == 0) {}
 
-    for (int i = 0; i < len; i++)
-    {
+    // Read the format string
+    for (int i = 0; i < len; i++) {
         format[i] = Serial.read();
     }
 }
 
-
-void PrintResult()#define LENGTH 100
-
-
-char buff[LENGTH];
-char format[LENGTH];
-
-
-void PrintWellcomeMessage();
-void inputFormat();
-void PrintResult(char* format);
-
-
-char pass[] = "Fundmentals_Of_Software_Security";
-
-
-void setup()
-{
-    Serial.begin(9600);
-}
-
-
-void loop()
-{
-    PrintWellcomeMessage();
-    delay(1000);
-
-    inputFormat();
-    delay(1000);
-    
-    PrintResult();
-
-    delay(1000);
-}
-
-
-void PrintWellcomeMessage()
-{
-    String welcomeMessage = "Welcome to format string CTF. Try to read the password from the stack";
-    Serial.println(welcomeMessage);
-    Serial.println("Enter the length of format string you want write");
-
-    delay(1000);
-}
-
-
-void inputFormat()
-{
-    while (Serial.available() == 0) {}
-
-    int len = Serial.parseInt();
-    
-    delay(10000);
-    Serial.println("Enter your format string:");
-    delay(1000);
-
-    while (Serial.available() == 0) {}
-
-    for (int i = 0; i < len; i++)
-    {
-        format[i] = Serial.read();
-    }
-}
-
-
-void PrintResult()
-{
+// Function to print the result of the password check
+void PrintResult() {
+    // Remove newline characters from the buffer
     buff[strcspn(buff, "\n")] = '\0';
     sprintf(buff, PSTR(format));
 
-    if (strncmp(pass, buff, sizeof(pass)))
-    {
+    // Check if the password matches
+    if (strncmp(pass, buff, sizeof(pass))) {
         Serial.print(buff);
         Serial.println(" does not have access");
-    }
-
-    else 
-    {
-        Serial.println("Good job!");
-    }
-}
-{
-    buff[strcspn(buff, "\n")] = '\0';
-    sprintf(buff, PSTR(format));
-
-    if (strncmp(pass, buff, sizeof(pass)))
-    {
-        Serial.print(buff);
-        Serial.println(" does not have access");
-    }
-
-    else 
-    {
+    } else {
         Serial.println("Good job!");
     }
 }
